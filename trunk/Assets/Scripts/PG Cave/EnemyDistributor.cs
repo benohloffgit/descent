e8945b0069f8111fc05bf5e6e8d2aee029e98e16
benni05 <benni05@1a9244a1-f8be-4099-9052-af0528f54d35>
2012-12-15 18:38:21 +0000

    Mine Touch and Editor placement of enemies

--- trunk/Assets/Scripts/PG Cave/EnemyDistributor.cs
+++ trunk/Assets/Scripts/PG Cave/EnemyDistributor.cs
@@ -34,31 +34,43 @@ public class EnemyDistributor {
 	}
 	
 	public void Distribute() {
-		
+				
 		IntTriple result = (IntTriple)emptyCells[UnityEngine.Random.Range(0,emptyCells.Count)];
 //		Debug.Log (result.x +" " + result.y +" " + result.z);
-		Vector3 pos = new Vector3(result.x * MarchingCubes.MESH_SCALE, result.y * MarchingCubes.MESH_SCALE, result.z * MarchingCubes.MESH_SCALE);
-		GameObject wallGun = GameObject.Instantiate(play.wallGunPrefab, pos, Quaternion.identity) as GameObject;
-		WallGun wG = wallGun.GetComponent<WallGun>();
-		wG.Initialize(game, play);
-		
+		Vector3 pos = new Vector3(result.x * MarchingCubes.MESH_SCALE, result.y * MarchingCubes.MESH_SCALE, result.z * MarchingCubes.MESH_SCALE);	
 		Vector3 rayPath = RandomVector();
-		//Debug.Log (rayPath);
-		if (Physics.Raycast(pos, rayPath, out hit, MAX_RAYCAST_DISTANCE, 1 << Game.LAYER_CAVE)) {
-			//Vector3 v1 = marchingCubes.mesh.vertices[marchingCubes.mesh.triangles[hit.triangleIndex * 3 + 0]];
-			//Vector3 v2 = marchingCubes.mesh.vertices[marchingCubes.mesh.triangles[hit.triangleIndex * 3 + 1]];
-			//Vector3 v3 = marchingCubes.mesh.vertices[marchingCubes.mesh.triangles[hit.triangleIndex * 3 + 2]];
-//			wallGun.transform.position = ((v1 + v2 + v3)/3) * MarchingCubes.MESH_SCALE;
-			
-			Vector3 centeredPos = hit.collider.transform.TransformPoint(hit.barycentricCoordinate);
-			wallGun.transform.position = pos + centeredPos;
-//			Debug.Log (centeredPos + " " + pos);
-			
-			wallGun.transform.forward = hit.normal;
+		
+		if (Physics.Raycast(pos, rayPath, out hit, MAX_RAYCAST_DISTANCE, 1 << Game.LAYER_CAVE)) {			
+			WallGun wallGun = CreateWallGun();
+			PlaceOnWall(wallGun.gameObject, hit);
 		}
 	}
+
+	public MineTouch CreateMineTouch() {
+		GameObject mT = GameObject.Instantiate(play.mineTouchPrefab) as GameObject;
+		MineTouch mineTouch = mT.GetComponent<MineTouch>();
+		mineTouch.Initialize(game, play);
+		return mineTouch;
+	}
 	
-	private Vector3 RandomVector() {
+	public WallGun CreateWallGun() {
+		GameObject wG = GameObject.Instantiate(play.wallGunPrefab) as GameObject;
+		WallGun wallGun = wG.GetComponent<WallGun>();
+		wallGun.Initialize(game, play);
+		return wallGun;
+	}
+	
+	public void PlaceOnWall(GameObject gO, RaycastHit h) {
+//		Vector3 centeredPos = h.collider.transform.TransformPoint(h.barycentricCoordinate);
+//		gO.transform.position = cubePosition + centeredPos;		
+		Vector3 v1 = marchingCubes.mesh.vertices[marchingCubes.mesh.triangles[h.triangleIndex * 3 + 0]];
+		Vector3 v2 = marchingCubes.mesh.vertices[marchingCubes.mesh.triangles[h.triangleIndex * 3 + 1]];
+		Vector3 v3 = marchingCubes.mesh.vertices[marchingCubes.mesh.triangles[h.triangleIndex * 3 + 2]];
+		gO.transform.position = ((v1 + v2 + v3)/3) * MarchingCubes.MESH_SCALE;
+		gO.transform.forward = h.normal;
+	}
+	
+	public static Vector3 RandomVector() {
 		return new Vector3(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value) * ((UnityEngine.Random.Range(0,2) == 0) ? 1 : -1);
 	}
 	

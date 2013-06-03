1024df535d508ebbf4dc3b65e3637a868297a914
Ben Ohloff <23360685+benohloffgit@users.noreply.github.com>
2013-06-03 20:41:37 +0000

    Exit Helper working

--- Assets/Scripts/Enemies/LightBulb.cs
+++ Assets/Scripts/Enemies/LightBulb.cs
@@ -1,30 +1,17 @@
 using UnityEngine;
 using System.Collections;
 
-public class LightBulb : Enemy {
-	private RaycastHit hit;
+public class LightBulb : Enemy {	
 	private GridPosition targetPosition;
 	
-	private static float FORCE_MOVE = 5.0f;
-	private static int LOOK_AT_DISTANCE = 4; // measured in cubes
-	private static float LOOK_AT_ANGLE_TOLERANCE = 30.0f;
-
-//	private static Vector3[] WEAPON_POSITIONS = new Vector3[] {new Vector3(0.53f, 0.63f, 0.750f), new Vector3(-0.53f, 0.63f, 0.750f)};
-		
 	public override void InitializeWeapon(int mount, int type) {
-/*		if (mount == Weapon.PRIMARY) {
-			primaryWeapons.Add(new Weapon(this, mount, transform, play, w, m, WEAPON_POSITIONS[0], Game.ENEMY, modelClazzAEquivalent + 1, spawn.isBoss));
-		} else {
-			secondaryWeapons.Add(new Weapon(this, mount, transform, play, w, m, WEAPON_POSITIONS[1], Game.ENEMY, modelClazzAEquivalent + 1, spawn.isBoss));
-		}*/
-	}
-	
-	void Start() {
-		targetPosition = play.cave.GetGridFromPosition(transform.position);
 	}
 		
 	public override void DispatchFixedUpdate(Vector3 isShipVisible) {		
-/*		play.movement.Roam(myRigidbody, currentGridPosition, ref targetPosition, 2, 4, FORCE_MOVE);
-		play.movement.LookAt(myRigidbody, currentGridPosition, play.ship.transform, LOOK_AT_DISTANCE, LOOK_AT_ANGLE_TOLERANCE, ref currentAngleUp, Movement.LookAtMode.IntoMovingDirection);*/
-	}		
+		play.movement.Roam(myRigidbody, currentGridPosition, ref targetPosition, roamMinRange, roamMaxRange, movementForce);
+		play.movement.LookAt(myRigidbody, play.ship.transform, 0, lookAtToleranceRoaming, ref currentAngleUp,
+			ref dotProductLookAt, Movement.LookAtMode.IntoMovingDirection);
+	}
+
 }
+

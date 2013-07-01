f6f6a284ed2606306caa7f49da2475e2f693656a
benni05 <benni05@1a9244a1-f8be-4099-9052-af0528f54d35>
2013-07-01 18:33:54 +0000

    Breadcrumbs new, minimap been-through doors and breadcrumbs

--- trunk/Assets/Scripts/PG Cave/Cell.cs
+++ trunk/Assets/Scripts/PG Cave/Cell.cs
@@ -7,6 +7,7 @@ public class Cell {
 	public bool isSpawn = false;
 	public bool isPowerUp = false;
 	public bool isKey = false;
+	public bool hasTransitionedBetweenRooms = false;
 
 	public Cell(IntTriple p, int mId, bool iE) {
 		pos = p;
